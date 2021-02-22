0d520a0964ba8f02527db95a35da24565b600c37
Paulo Vitor <paulovitor2123@gmail.com>
2021-02-22 18:24:13 -0300

    First routes

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,14 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3333, () => console.log('teste 2 2'));
+
+app.get('/', (request, response) => {
+    return response.json({ message: "Hello World - NLW04" });
+});
+
+app.post('/', (request, response) => {
+    return response.json({ message: "Os dados forma salvos com sucesso!" });
+});
+
+app.listen(3333, () => console.log('starting in a port 3333'));
+
