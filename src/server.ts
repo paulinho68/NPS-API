d3c48ccf96b286726e4dc7f21a4064a4640e61af
Paulo Vitor <paulovitor2123@gmail.com>
2021-02-24 01:28:50 -0300

    Add Controller and Models with typeOrm

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,12 @@
-import express from 'express';
 import "reflect-metadata";
+import "./database";
+import express from 'express';
+import { router } from './routes';
 
 const app = express();
 
-
-app.get('/', (request, response) => {
-    return response.json({ message: "Hello World - NLW04" });
-});
-
-app.post('/', (request, response) => {
-    return response.json({ message: "Os dados forma salvos com sucesso!" });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log('starting in a port 3333'));
 
