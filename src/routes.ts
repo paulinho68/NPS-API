6f7c06647b5bfb46ed372de0f30578c84bf5fd22
Paulo Vitor <paulovitor2123@gmail.com>
2021-02-26 15:30:41 -0300

    Now is possible set the avaliation trought email

--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import { SurveyController } from './controllers/SurveyController';
 import { UserController } from "./controllers/UserController";
 import { SendMailController } from "./controllers/SendMailController";
+import { AnswerController } from "./controllers/AnswerController";
 
 const router = Router();
 
 const userController = new UserController();
 const surveyController = new SurveyController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
 
 router.post('/users', userController.create);
 
@@ -15,5 +17,6 @@ router.post('/surveys', surveyController.create);
 router.get('/surveys', surveyController.index);
 
 router.post('/sendMail', sendMailController.execute);
+router.get('/answers/:value', answerController.execute);
 
 export { router };
